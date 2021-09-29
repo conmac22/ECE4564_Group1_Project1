bee0bcf9e7682a1ffa95b009d2182ad3b3416f4d
seanq9 <54922943+seanq9@users.noreply.github.com>
2021-09-28 21:12:02 -0400

    Add files via upload

---
+++ key.py
@@ -0,0 +1,3 @@
+watsonKey = 'EUOrEO015oZ0rBlILjh3i8FgCq_Hhc9Woz1dn-PeTXbG'
+watsonURL = 'https://api.us-east.text-to-speech.watson.cloud.ibm.com/instances/d1180113-b514-46c0-823c-3c542ed0425b'
+app_id = 'WUXT5T-ATAW29X3TH'
